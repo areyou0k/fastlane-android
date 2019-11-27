fe7849f55eaa506e05020efb8ff111e53047854a
BruceLEO1969 <bruceleo1969@gmail.com>
2019-11-27 11:28:35 +0800

    story and bug.

--- fastlane/actions/jirastatus.rb
+++ fastlane/actions/jirastatus.rb
@@ -19,7 +19,7 @@ module Fastlane
               issue = item_strip.gsub(/.*\(https:\/\/jira.hellotalk8.com\/jira\/browse\//, '').gsub(/\)/, '')
               puts issue
               self.change_assignee(issue)
-              self.change_workflow_status(issue)
+              # self.change_workflow_status(issue)
             end
           end
         end
@@ -31,96 +31,64 @@ module Fastlane
       #   require 'uri'
       #   require 'json'
       #   api = "https://jira.hellotalk8.com/jira/rest/api/2/issue/"
-      #   url = api + "#{issue['issue']}" + "/transitions"
+      #   url = api + "#{issue}" + "/transitions"
+      #   puts "Issue url:"
       #   puts url
       #   uri = URI(url)
       #   header = {"Content-Type": "application/json"}
-      #   workflow_id = {"transition":{"id":"91"}}
-      
+      #   workflow_id_built = {"transition":{"id":"91"}}
+      #   workflow_id_PV = {"transition":{"id":"211"}}
+
       #   Net::HTTP.start(uri.host, uri.port,
-      #     :use_ssl => uri.scheme == 'https', 
+      #     :use_ssl => uri.scheme == 'https',
       #     :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-        
+
       #     request = Net::HTTP::Post.new(uri.request_uri, header)
-      #     request.body = workflow_id.to_json
+      #     request.body = workflow_id_built.to_json
       #     request.basic_auth('zhangfeng', '12345678')
-        
+
       #     response = http.request(request)
-      #     puts "Change workflow to BUILT(id: 91)."
-      #     puts response.code
-      #   end 
+      #     puts "Change workflow to BUILT status code: #{response.code}"
+      #     if response.code == "204"
+      #       request = Net::HTTP::Post.new(uri.request_uri, header)
+      #       request.body = workflow_id_PV.to_json
+      #       request.basic_auth('zhangfeng', '12345678')
+      #       response = http.request(request)
+      #       puts "Change workflow to Product Verify status code: #{response.code}"
+      #       if response.code == "204"
+      #         puts "Change workflow to Product Verify(id: 211)."
+      #       else
+      #         puts "Did not change to PV."
+      #       end
+      #     else
+      #       puts "Workflow didn't change."
+      #     end
+      #   end
       # end
 
-      def self.change_workflow_status(issue)
+      def self.change_workflow_status(issue, workflow)
         require 'net/http'
         require 'net/https'
         require 'uri'
         require 'json'
         api = "https://jira.hellotalk8.com/jira/rest/api/2/issue/"
         url = api + "#{issue}" + "/transitions"
-        puts "Issue url:"
-        puts url
         uri = URI(url)
         header = {"Content-Type": "application/json"}
-        workflow_id_built = {"transition":{"id":"91"}}
-        workflow_id_PV = {"transition":{"id":"211"}}
+        workflow_id = {"transition":{"id":"#{workflow}"}}
 
         Net::HTTP.start(uri.host, uri.port,
           :use_ssl => uri.scheme == 'https',
           :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
 
           request = Net::HTTP::Post.new(uri.request_uri, header)
-          request.body = workflow_id_built.to_json
+          request.body = workflow_id.to_json
           request.basic_auth('zhangfeng', '12345678')
 
           response = http.request(request)
-          puts "Change workflow to BUILT status code: #{response.code}"
-          if response.code == "204"
-            request = Net::HTTP::Post.new(uri.request_uri, header)
-            request.body = workflow_id_PV.to_json
-            request.basic_auth('zhangfeng', '12345678')
-            response = http.request(request)
-            puts "Change workflow to Product Verify status code: #{response.code}"
-            if response.code == "204"
-              puts "Change workflow to Product Verify(id: 211)."
-            else
-              puts "Did not change to PV."
-            end
-          else
-            puts "Workflow didn't change."
-          end
+          puts "Change workflow status code: #{response.code}"
         end
       end
-
-      # def self.change_assignee(issue)
-      #   require 'net/http'
-      #   require 'net/https'
-      #   require 'uri'
-      #   require 'json'
-      #   api = "https://jira.hellotalk8.com/jira/rest/api/2/issue/"
-      #   url = api + "#{issue}"
-      #   puts url
-      #   uri = URI(url)
-      #   header = {"Content-Type": "application/json"}
-      #   if "#{issue['user']}" != nil
-      #     assig = {"update": {"assignee": [{"set": {"name": "#{issue['user']}"}}]}}
-      
-      #     Net::HTTP.start(uri.host, uri.port,
-      #       :use_ssl => uri.scheme == 'https', 
-      #       :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-          
-      #       request = Net::HTTP::Put.new(uri.request_uri, header)
-      #       request.body = assig.to_json
-      #       puts request.body
-      #       request.basic_auth('zhangfeng', '12345678')
-          
-      #       response = http.request(request)
-            
-      #       puts "Change assignee to #{issue['user']}."
-      #       puts response.code
-      #     end
-      #   end   
-      # end
 
       def self.change_assignee(issue)
         require 'net/http'
@@ -146,30 +114,57 @@ module Fastlane
           full_json = JSON.parse(response.body)
           puts "Get assignee info status code: #{response.code}"
 
-          if full_json['fields']['customfield_10521']
-            pm = full_json['fields']['customfield_10521']['name']
-          end
+          if full_json['fields']['issuetype']['name'] == "Story"
+            puts "Issue #{issue} type is: Story."
+            #workflow "Passed Build" statu id
+            workflow = "91"
+            puts "Change status to: Passed Build."
+            change_workflow_status(issue, workflow)
+            #workflow "Product Verify" statu id
+            workflow = "211"
+            puts "Change status to: Product Verify."
+            change_workflow_status(issue, workflow)
 
-          if full_json['fields']['customfield_10514']
-            data = full_json['fields']['customfield_10514']['name']
-          end
+            if full_json['fields']['customfield_10521']
+              pm = full_json['fields']['customfield_10521']['name']
+            end
 
-          if full_json['fields']['customfield_10515']
-            operation = full_json['fields']['customfield_10515']['name']
-          end
+            if full_json['fields']['customfield_10514']
+              data = full_json['fields']['customfield_10514']['name']
+            end
 
-          if full_json['fields']['reporter']
-            reporter = full_json['fields']['reporter']['name']
-          end
+            if full_json['fields']['customfield_10515']
+              operation = full_json['fields']['customfield_10515']['name']
+            end
 
-          if pm
-            user = pm
-          elsif data
-            user = data
-          elsif operation
-            user = operation
+            if full_json['fields']['reporter']
+              reporter = full_json['fields']['reporter']['name']
+            end
+
+            if pm
+              user = pm
+            elsif data
+              user = data
+            elsif operation
+              user = operation
+            else
+              user = reporter
+            end
+          elsif
+            full_json['fields']['issuetype']['name'] == "Story Bugs"
+            puts "Issue #{issue} type is: Story Bugs."
+            user = "sunshine"
+            #workflow "Story Bug Fixed" statu id
+            workflow = "11"
+            puts "Change status to: Story Bug Fixed."
+            change_workflow_status(issue, workflow)
           else
-            user = reporter
+            puts "Issue #{issue} type is: Bugs."
+            user = full_json['fields']['reporter']['name']
+            #workflow "Passed Build" statu id
+            workflow = "91"
+            puts "Change status to: Passed Build."
+            change_workflow_status(issue, workflow)
           end
 
           assignee = {"update": {"assignee": [{"set": {"name": "#{user}"}}]}}
@@ -186,6 +181,71 @@ module Fastlane
           puts "Set assignee status code: #{response.code}"
         end
       end
+
+      # def self.change_assignee(issue)
+      #   require 'net/http'
+      #   require 'net/https'
+      #   require 'uri'
+      #   require 'json'
+      #   api = "https://jira.hellotalk8.com/jira/rest/api/2/issue/"
+      #   url = api + "#{issue}"
+      #   puts "Issue url:"
+      #   puts url
+      #   uri = URI(url)
+      #   header = {"Content-Type": "application/json"}
+
+      #   Net::HTTP.start(uri.host, uri.port,
+      #     :use_ssl => uri.scheme == 'https', 
+      #     :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+      #     #Get assignee info.
+      #     request = Net::HTTP::Get.new(uri.request_uri, header)
+      #     request.basic_auth('zhangfeng', '12345678')
+
+      #     response = http.request(request)
+      #     full_json = JSON.parse(response.body)
+      #     puts "Get assignee info status code: #{response.code}"
+
+      #     if full_json['fields']['customfield_10521']
+      #       pm = full_json['fields']['customfield_10521']['name']
+      #     end
+
+      #     if full_json['fields']['customfield_10514']
+      #       data = full_json['fields']['customfield_10514']['name']
+      #     end
+
+      #     if full_json['fields']['customfield_10515']
+      #       operation = full_json['fields']['customfield_10515']['name']
+      #     end
+
+      #     if full_json['fields']['reporter']
+      #       reporter = full_json['fields']['reporter']['name']
+      #     end
+
+      #     if pm
+      #       user = pm
+      #     elsif data
+      #       user = data
+      #     elsif operation
+      #       user = operation
+      #     else
+      #       user = reporter
+      #     end
+
+      #     assignee = {"update": {"assignee": [{"set": {"name": "#{user}"}}]}}
+
+      #     #set assignee.
+      #     request = Net::HTTP::Put.new(uri.request_uri, header)
+      #     request.body = assignee.to_json
+      #     puts "assignee josn: #{request.body}"
+      #     request.basic_auth('zhangfeng', '12345678')
+
+      #     response = http.request(request)
+
+      #     puts "Change assignee to #{user}."
+      #     puts "Set assignee status code: #{response.code}"
+      #   end
+      # end
 
       def self.description
         "Change jira workflow status and assignee."
